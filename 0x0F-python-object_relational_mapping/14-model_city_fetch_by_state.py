a59e03e458b13c3b4e5b7f3e23aa23ae94116812
Ron David <ololoron@gmail.com>
2024-02-25 09:45:15 +0300

    things fall into place"

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    rows = session.query(City, State).filter(City.state_id == \
-        State.id).order_by(City.id).all()
+    rows = session.query(City, State).filter(City.state_id == State.id)\
+        .order_by(City.id).all()
     for city, state in rows:
         print("{}:({}) {}".format(state.name, city.id, city.name))
     session.close()
