f1023b94d301cbb61f18b4f054eac26d0341b601
Ron David <ololoron@gmail.com>
2024-02-26 17:47:25 +0300

    Loop to Languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints 3 lines but only an array of string and a loop
 
-const threeLangs = ['C in fun', 'Python is cool', 'JavaScript is amazing'];
+const threeLangs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(threeLangs[i]);
